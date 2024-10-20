096fe33d5d553632b70992abacbcad6a551aa738
sy <3078567417@qq.com>
2024-10-20 15:08:05 +0800

    "用户模块审查"

--- src/main/java/com/kob/backend/service/PostFavourService.java
+++ src/main/java/com/kob/backend/service/PostFavourService.java
@@ -11,8 +11,7 @@ import com.kob.backend.model.entity.User;
 /**
  * 帖子收藏服务
  *
- * @author <a href="https://github.com/liyupi">程序员鱼皮</a>
- * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ * @author sy
  */
 public interface PostFavourService extends IService<PostFavour> {
 
