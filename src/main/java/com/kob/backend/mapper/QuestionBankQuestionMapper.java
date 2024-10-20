c04466c1abebad3310676f84fbeaa0d75f0a0316
sy <3078567417@qq.com>
2024-10-20 14:55:50 +0800

    初始化项目

---
+++ src/main/java/com/kob/backend/mapper/QuestionBankQuestionMapper.java
@@ -0,0 +1,18 @@
+package com.kob.backend.mapper;
+
+import com.kob.backend.model.entity.QuestionBankQuestion;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+* @author 诺诺
+* @description 针对表【question_bank_question(题库题目)】的数据库操作Mapper
+* @createDate 2024-10-19 10:06:12
+* @Entity com.kob.backend.model.entity.QuestionBankQuestion
+*/
+public interface QuestionBankQuestionMapper extends BaseMapper<QuestionBankQuestion> {
+
+}
+
+
+
+
